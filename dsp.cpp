7425232baddbfd21e815fd8ab5d3043374608170
fredjpelon <fred.pelon@gmail.com>
2024-01-01 15:17:30 -0700

    CPP DSP
    add rough class to get started on classes

--- dsp.cpp
+++ dsp.cpp
@@ -3,29 +3,86 @@
 
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <cstdlib>
+//#include <cstring>
+//#include <cstdlib>
 
 using namespace std;
-class Wavfile
-{
-public:
-    void ReadHeader();
-};
 
 #define BUF_SIZE 4
 #define D_BUF_SIZE 2
 
+class Wavfile
+{
+private:
+    char m_buf[BUF_SIZE]={0,0};
+    //m_buf = new char [BUF_SIZE];
+public:
+    void ReadHeader(const char* fname)
+    {
+        // read in a wav file
+        cout << fname << endl;
+        ifstream ifs(fname,ios::binary);
+        if(!ifs)
+        {
+            cout << "couldn't open target for input\n";
+            //return 1;
+        }
+
+        // read header
+        ifs.read(m_buf,BUF_SIZE); // RIFF
+        cout << string(&m_buf[0], BUF_SIZE) << endl;
+        ifs.read(m_buf,BUF_SIZE); // file size
+        int file_len = *reinterpret_cast<int*>(m_buf);
+        file_len += 8; // size on disk
+        cout << "file_len = " << file_len << endl;
+        ifs.read(m_buf,BUF_SIZE); // WAVE
+        cout << string(&m_buf[0], BUF_SIZE) << endl;
+        ifs.read(m_buf,BUF_SIZE); // fmt chunk marker
+        cout << string(&m_buf[0], BUF_SIZE) << endl;
+        ifs.read(m_buf,BUF_SIZE); // length of format data
+        cout << "len of format data = " << *reinterpret_cast<int*>(m_buf) << endl;
+        ifs.read(m_buf,BUF_SIZE); // data format and number of channels
+        int d = *reinterpret_cast<int*>(m_buf);
+        int data_format = d >> 16;
+        int num_channels = d & 0xffff;
+        cout << "data format = " << data_format << endl;
+        cout << "number of channels = " << num_channels << endl;
+        ifs.read(m_buf,BUF_SIZE); // sample rate
+        int sample_rate = *reinterpret_cast<int*>(m_buf);
+        cout << "sample rate = " << sample_rate << " Hz" << endl;
+        ifs.read(m_buf,BUF_SIZE); // byte rate
+        int byte_rate = *reinterpret_cast<int*>(m_buf);
+        cout << "byte_rate = " << byte_rate << endl;
+        ifs.read(m_buf,BUF_SIZE); // rec type and bps
+        d = *reinterpret_cast<int*>(m_buf);
+        int bits_per_sample = d >> 16;
+        int rec_type= d & 0xffff;
+        cout << "recording type = " << rec_type << endl;
+        cout << "bits per sample = " << bits_per_sample << endl;
+        ifs.read(m_buf,BUF_SIZE); // data
+        cout << string(&m_buf[0], BUF_SIZE) << endl;
+        ifs.read(m_buf,BUF_SIZE); // data len
+        int data_len_in_bytes = *reinterpret_cast<int*>(m_buf);
+        int data_len = data_len_in_bytes/(bits_per_sample/8);
+        cout << "data_len = " << data_len << endl;        
+
+        ifs.close();
+    }
+};
+
 int main()
 {
-    char *buf;
-    buf = new char [BUF_SIZE];
+    //char *buf;
+    //buf = new char [BUF_SIZE];
+    Wavfile wavfile;
 
     string fname = "JFJF_2023126_11158_2.wav";
-    string playcmd = "aplay " + fname;
+    //string playcmd = "aplay " + fname;
 
-    //wavfile.ReadHeader(fname);
+    wavfile.ReadHeader(fname.c_str());
 
+    return 0;
+/*
     cout << "Digital Signal Processing in C++\n";
     //system(playcmd.c_str()); // system() expects a constant
 
@@ -100,5 +157,5 @@ int main()
     }
 
 
-    ifs.close();
+    ifs.close();*/
 }
